2f4f4c35f4366fe61a85900a290aa4fcebc656b8
Ryan leong <rjleong2002@gmail.com>
2023-05-20 13:13:38 +0800

    stuff

--- videdoeditor_copy.py
+++ videdoeditor_copy.py
@@ -9,28 +9,31 @@ from moviepy.video.tools.segmenting import findObjects
 def videointromaker(filename):
 	title = filename.strip(".mp4")
 	# screen size
-	screensize = (720, 460)
+	screensize = (1560, 1080)
 	
 	# creating a text clip of color green, font is Arial and size is 80
 	txtClip = TextClip(title, color = 'gold', font = "Monteseratt",
-					kerning = 5, fontsize = 80)
+					kerning = 5, fontsize = 50)
 
 	title_clip = txtClip
 
 	template_clip = VideoFileClip("templates/template.mp4").subclip(0,5)
 
 
-	intro_video = CompositeVideoClip([template_clip, title_clip.subclip(0,5).set_pos('center').crossfadein(1.5).set_start(1.7).set_duration(3).crossfadeout(.5)])
-	mid_clip = VideoFileClip(f"vids_to_edit/{filename}")
-	end_clip = VideoFileClip("templates/template_end.mp4")
+	intro_video = CompositeVideoClip([template_clip, title_clip.subclip(0,5).set_pos('center').crossfadein(1.5).set_start(1.7).set_duration(3).crossfadeout(.5)]).resize(screensize)
+	mid_clip = VideoFileClip(f"vids_to_edit/{filename}").resize(screensize)
+	end_clip = VideoFileClip("templates/template_end.mp4").resize(screensize)
 	final_video = concatenate_videoclips([intro_video,mid_clip,end_clip])
 
 
-	final_video.write_videofile(f"vids_final/{filename}",fps=60)
+	final_video.write_videofile(f"vids_final/{filename}",fps=30)
 
 # Get the list of all files and directories
 
 dir_list = os.listdir("./vids_to_edit")
 
 for videoname in dir_list:
+	videoname = videoname.replace("Peter Tan ", "").strip()
+    
+
 	videointromaker(videoname)
